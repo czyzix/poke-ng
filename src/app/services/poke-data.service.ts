f97beaaea9b592496ad3f349b6d52061b3f4caf2
czyzix <czyzevsky@gmail.com>
2023-06-16 11:39:31 +0200

    Fetching pokemon data

---
+++ src/app/services/poke-data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeDataService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getPokemonData() {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=9`)
+  }
+
+  getBasicPokemonData(name: string) {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  }
+}
