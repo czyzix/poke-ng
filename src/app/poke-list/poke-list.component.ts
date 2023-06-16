e8b5c76d642e1cf7219f2614d8972f39c1be4573
czyzix <czyzevsky@gmail.com>
2023-06-16 11:40:20 +0200

    Creating poke-list component

---
+++ src/app/poke-list/poke-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeDataService } from '../services/poke-data.service';
+
+@Component({
+  selector: 'app-poke-list',
+  templateUrl: './poke-list.component.html',
+  styleUrls: ['./poke-list.component.css']
+})
+export class PokeListComponent implements OnInit {
+
+  pokemons: any[] = [];
+
+  constructor(
+    private pokeDataService: PokeDataService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokeDataService.getPokemonData()
+      .subscribe((response: any) => {response.results.forEach((result: any) => {
+        this.pokeDataService.getBasicPokemonData(result.name)
+          .subscribe((uniqResponse: any) => {
+            this.pokemons.push(uniqResponse);
+            
+          })
+      })})
+
+      console.log(this.pokemons)
+  }
+
+}
